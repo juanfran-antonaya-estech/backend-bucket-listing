921bf3726d74e49d511c4aa1c361b518640dca34
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-05-02 13:13:46 +0200

    Integración de session key

--- src/routes/feed.routes.js
+++ src/routes/feed.routes.js
@@ -3,8 +3,22 @@ import {pool} from "../db.js";
 
 const router = Router()
 
-router.get("/api/feed", async(req, res) => {
-    const user = 2
+router.get("/api/feed", async (req, res) => {
+
+    const {session_id} = req.headers
+    if(session_id == undefined){
+        res.status(405).json({
+            "message" : "no me vale eso"
+        })
+        return
+    }
+    const [session_key] = await pool.query("SELECT pf.id, pv.session_id FROM profile AS pf\n" +
+        "JOIN usuario AS lg ON pf.user_id = lg.id\n" +
+        "JOIN pivot_login_user AS pv ON pv.user_id = lg.id\n" +
+        `WHERE pv.session_id = '${session_id}';`)
+    const user = session_key[0].id
+
+
     const [results] = await pool.query("SELECT pv.thingo_id AS id_thingo, pv.profile_id AS id_de_quien_lo_ha_hecho, th.name AS nombre_thingo, other.apodo AS nombre_de_quien_lo_ha_hecho, pv.last_done_date AS fecha_hecho_por_otro FROM pivot_thingos_perfil AS pv\n" +
         "JOIN thingos AS th ON pv.thingo_id = th.id\n" +
         "JOIN profile AS other ON pv.profile_id = other.id\n" +
@@ -23,14 +37,14 @@ router.get("/api/feed", async(req, res) => {
     res.json(results)
 })
 
-router.get("/api/trending", async(req, res) => {
-    const [trending] = await pool.query("SELECT pv.thingo_id, th.name, cat.name, cat.image, COUNT(react.id) AS puntos_de_insanidad FROM pivot_thingos_perfil as pv\n" +
-        "JOIN thingos AS th ON pv.thingo_id = th.id\n" +
-        "JOIN cathingory AS cat ON th.cathingory_id = cat.id\n" +
-        "LEFT JOIN reacthingos AS react ON (react.pivot_id = pv.id) AND (react.reaction_type = 'queloco')\n" +
-        "WHERE pv.last_done_date >= CURRENT_DATE - INTERVAL '7' DAY\n" +
-        "GROUP BY pv.id\n" +
-        "ORDER BY COUNT(react.id) DESC;")
+router.get("/api/trending", async (req, res) => {
+    const [trending] = await pool.query("SELECT pv.thingo_id, th.name AS nombre_thingo, cat.name AS nombre_categoria, cat.image AS imagen_categoria, COUNT(react.id) AS puntos_de_insanidad FROM pivot_thingos_perfil as pv\n" +
+        "        JOIN thingos AS th ON pv.thingo_id = th.id\n" +
+        "        JOIN cathingory AS cat ON th.cathingory_id = cat.id\n" +
+        "        LEFT JOIN reacthingos AS react ON (react.pivot_id = pv.id) AND (react.reaction_type = 'queloco')\n" +
+        "        WHERE pv.last_done_date >= CURRENT_DATE - INTERVAL '7' DAY\n" +
+        "        GROUP BY th.id\n" +
+        "        ORDER BY COUNT(react.id) DESC;")
     res.json(trending)
 })
 
