f51346dc0e7a5322c259add7052ab6e006c282cc
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-05-02 11:55:33 +0200

    Feed hecho

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cathingoryRoutes from "./routes/cathingory.routes.js";
 import profileRoutes from "./routes/profile.routes.js";
+import feedRoutes from "./routes/feed.routes.js";
 
 const app = express()
 
@@ -9,5 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(cathingoryRoutes)
 app.use(profileRoutes)
+app.use(feedRoutes)
 
 app.listen(3000)
