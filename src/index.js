62cde08a7bc66887b60e9dde6a6a0b6329225540
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-05-06 09:59:47 +0200

    Implementando el login

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cathingoryRoutes from "./routes/cathingory.routes.js";
 import profileRoutes from "./routes/profile.routes.js";
 import feedRoutes from "./routes/feed.routes.js";
+import loginRoutes from "./routes/login.routes.js";
 
 const app = express()
 
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use(cathingoryRoutes)
 app.use(profileRoutes)
 app.use(feedRoutes)
+app.use(loginRoutes)
 
 app.listen(3000)
